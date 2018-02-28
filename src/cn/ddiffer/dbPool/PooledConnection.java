c969dc36251a756d39ddb5c20a440a6ed3f48487
zidong <zidong.me@gmail.com>
2018-02-28 23:49:27 +0800

    增加配置文件 db.properties

--- src/cn/ddiffer/dbPool/PooledConnection.java
+++ src/cn/ddiffer/dbPool/PooledConnection.java
@@ -5,6 +5,34 @@ package cn.ddiffer.dbPool;
  * Created by Zidong on 2018/2/26 18:52
  */
 public class PooledConnection implements IMyPool {
+    // 数据库驱动
+    private String jdbcDriver = "";
+
+    // 数据库连接参数
+    private String jdbcUrl = "";
+
+    private String userName = "";
+
+    private String password = "";
+
+    // 连接池规格
+    private static int initCount;
+
+    private static int stepSize;
+
+    private static int maxSize;
+
+    public PooledConnection() {
+        init();
+    }
+
+    /**
+     * 初始化方法
+     */
+    private void init() {
+
+    }
+
 
     @Override
     public PooledConnection getConnection() {
